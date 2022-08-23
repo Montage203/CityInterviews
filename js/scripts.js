c3a661ea75bbb6da4c643964d49c08a37c82f8ae
Montage203 <76145046+Montage203@users.noreply.github.com>
2022-08-23 23:30:01 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,210 +1,484 @@
 
 
 
-    let togg0 = document.getElementById("togg0");
-    let togg1 = document.getElementById("togg1");
-    let togg2 = document.getElementById("togg2");
-    let togg3 = document.getElementById("togg3");
-    let togg4 = document.getElementById("togg4");
-    let d0 = document.getElementById("d0");
-    let d1 = document.getElementById("d1");
-    let d2 = document.getElementById("d2");
-    let d3 = document.getElementById("d3");
-    let d4 = document.getElementById("d4");
-    $(document).ready(function(){
-      $(togg0).click(function(){
-        if(getComputedStyle(d0).display != "none"){
-          $(d0).animate({
-            height: 'toggle'
-          });
-          d1.style.display = "none";
-          d2.style.display = "none";
-          d3.style.display = "none";
-          d4.style.display = "none";
+let togg0 = document.getElementById("togg0");
+let togg1 = document.getElementById("togg1");
+let togg2 = document.getElementById("togg2");
+let togg3 = document.getElementById("togg3");
+let togg4 = document.getElementById("togg4");
+let togg5 = document.getElementById("togg5");
+let togg6 = document.getElementById("togg6");
+let togg7 = document.getElementById("togg7");
+let togg8 = document.getElementById("togg8");
+let togg9 = document.getElementById("togg9");
+let togg10 = document.getElementById("togg10");
+let d0 = document.getElementById("d0");
+let d1 = document.getElementById("d1");
+let d2 = document.getElementById("d2");
+let d3 = document.getElementById("d3");
+let d4 = document.getElementById("d4");
+let d5 = document.getElementById("d5");
+let d6 = document.getElementById("d6");
+let d7 = document.getElementById("d7");
+let d8 = document.getElementById("d8");
+let d9 = document.getElementById("d9");
+let d10 = document.getElementById("d10");
+$(document).ready(function () {
+  $(togg0).click(function () {
+    if (getComputedStyle(d0).display != "none") {
+      $(d0).animate({
+        height: 'toggle'
+      });
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      $(d0).animate({
+        height: 'toggle'
+      });
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+$(document).ready(function () {
+  $(togg1).click(function () {
+    if (getComputedStyle(d1).display != "none") {
+      d0.style.display = "none";
+      $(d1).animate({
+        height: 'toggle'
+      });
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      $(d1).animate({
+        height: 'toggle'
+      });
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+$(document).ready(function () {
+  $(togg2).click(function () {
+    if (getComputedStyle(d2).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      $(d2).animate({
+        height: 'toggle'
+      });
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      $(d2).animate({
+        height: 'toggle'
+      });
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+$(document).ready(function () {
+  $(togg3).click(function () {
+    if (getComputedStyle(d3).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      $(d3).animate({
+        height: 'toggle'
+      });
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      //d3.style.display = "block";
+      $(d3).animate({
+        height: 'toggle'
+      });
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+$(document).ready(function () {
+  $(togg4).click(function () {
+    if (getComputedStyle(d4).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      $(d4).animate({
+        height: 'toggle'
+      });
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      $(d4).animate({
+        height: 'toggle'
+      });
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(togg5).click(function () {
+    if (getComputedStyle(d5).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      $(d5).animate({
+        height: 'toggle'
+      });
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      $(d5).animate({
+        height: 'toggle'
+      });
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(togg6).click(function () {
+    if (getComputedStyle(d6).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      $(d6).animate({
+        height: 'toggle'
+      });
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      $(d6).animate({
+        height: 'toggle'
+      });
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(togg7).click(function () {
+    if (getComputedStyle(d7).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      $(d7).animate({
+        height: 'toggle'
+      });
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      $(d7).animate({
+        height: 'toggle'
+      });
+      d8.style.display = "none";
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(togg8).click(function () {
+    if (getComputedStyle(d8).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      $(d8).animate({
+        height: 'toggle'
+      });
+      d9.style.display = "none";
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      $(d8).animate({
+        height: 'toggle'
+      });
+      d9.style.display = "none";
+      d10.style.display = "none";
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(togg9).click(function () {
+    if (getComputedStyle(d9).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      $(d9).animate({
+        height: 'toggle'
+      });
+      d10.style.display = "none";
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      $(d9).animate({
+        height: 'toggle'
+      });
+      d10.style.display = "none";
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(togg10).click(function () {
+    if (getComputedStyle(d10).display != "none") {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      $(d10).animate({
+        height: 'toggle'
+      });
+    } else {
+      d0.style.display = "none";
+      d1.style.display = "none";
+      d2.style.display = "none";
+      d3.style.display = "none";
+      d4.style.display = "none";
+      d5.style.display = "none";
+      d6.style.display = "none";
+      d7.style.display = "none";
+      d8.style.display = "none";
+      d9.style.display = "none";
+      $(d10).animate({
+        height: 'toggle'
+      });
+    }
+  });
+});
+
+
+
+// //
+
+
+
+
+$(document).ready(function () {
+
+  //DEBOUNCE
+
+  debounce = function (func, wait, immediate) {
+    var timeout;
+    return function () {
+      var context = this, args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  //SCROLL ANIMATED
+
+  $(function () {
+    var $target = $('[data-anime="scroll"],[data-anime-top="scroll-top"],[data-anime-left="scroll-left"],[data-anime-right="scroll-right"],[data-anime-bottom="scroll-bottom"]'),
+      animationClass = 'animate',
+      offset = $(window).height() * 3 / 4;
+
+    function animeScroll() {
+      var documentTop = $(document).scrollTop();
+      $target.each(function () {
+        var itemTop = $(this).offset().top;
+
+        if (documentTop > itemTop - offset) {
+          $(this).addClass(animationClass);
         } else {
-          $(d0).animate({
-            height: 'toggle'
-          });
-          d1.style.display = "none";
-          d2.style.display = "none";
-          d3.style.display = "none";
-          d4.style.display = "none";
+          $(this).removeClass(animationClass);
         }
       });
-    });
-    $(document).ready(function(){
-      $(togg1).click(function(){
-        if(getComputedStyle(d1).display != "none"){
-          d0.style.display = "none";
-          $(d1).animate({
-            height: 'toggle'
-          });
-          d2.style.display = "none";
-          d3.style.display = "none";
-          d4.style.display = "none";
-        } else {
-          d0.style.display = "none";
-          $(d1).animate({
-            height: 'toggle'
-          });
-          d2.style.display = "none";
-          d3.style.display = "none";
-          d4.style.display = "none";
-        }
-      });
-    });
-    $(document).ready(function(){
-      $(togg2).click(function(){
-        if(getComputedStyle(d2).display != "none"){
-          d0.style.display = "none";
-          d1.style.display = "none";
-          $(d2).animate({
-            height: 'toggle'
-          });
-          d3.style.display = "none";
-          d4.style.display = "none";
-        } else {
-          d0.style.display = "none";
-          d1.style.display = "none";
-          $(d2).animate({
-            height: 'toggle'
-          });
-          d3.style.display = "none";
-          d4.style.display = "none";
-        }
-      });
-    });
-    $(document).ready(function(){
-      $(togg3).click(function(){
-        if(getComputedStyle(d3).display != "none"){
-          d0.style.display = "none";
-          d1.style.display = "none";
-          d2.style.display = "none";
-          $(d3).animate({
-            height: 'toggle'
-          });
-          d4.style.display = "none";
-        } else {
-          d0.style.display = "none";
-          d1.style.display = "none";
-          d2.style.display = "none";
-          //d3.style.display = "block";
-          $(d3).animate({
-            height: 'toggle'
-          });
-          d4.style.display = "none";
-        }
-      });
-    });
-    $(document).ready(function(){
-      $(togg4).click(function(){
-        if(getComputedStyle(d4).display != "none"){
-          d0.style.display = "none";
-          d1.style.display = "none";
-          d2.style.display = "none";
-          d3.style.display = "none";
-          $(d4).animate({
-            height: 'toggle'
-          });
-        } else {
-          d0.style.display = "none";
-          d1.style.display = "none";
-          d2.style.display = "none";
-          d3.style.display = "none";
-          $(d4).animate({
-            height: 'toggle'
-          });
-        }
-      });
-    });
-   
-    
-    
- 
+    }
 
-    // //
+    animeScroll();
+
+    $(document).scroll(debounce(function () {
+      animeScroll();
+    }, 200));
+
+  });
+
+
+});
 
 
 
-
-    $(document).ready(function(){
-
-      //DEBOUNCE
-      
-      debounce = function(func, wait, immediate) {
-      var timeout;
-      return function() {
-        var context = this, args = arguments;
-        var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-        };
-      }; 
-        
-      //SCROLL ANIMATED
-      
-      $(function(){
-        var $target = $('[data-anime="scroll"],[data-anime-top="scroll-top"],[data-anime-left="scroll-left"],[data-anime-right="scroll-right"],[data-anime-bottom="scroll-bottom"]'),
-          animationClass = 'animate',
-          offset = $(window).height() * 3/4;
-      
-        function animeScroll(){
-          var documentTop = $(document).scrollTop();
-          $target.each(function(){
-            var itemTop = $(this).offset().top;
-            
-            if (documentTop > itemTop - offset){
-              $(this).addClass(animationClass);
-            } else {
-              $(this).removeClass(animationClass);
-            }
-          });
-        }
-    
-        animeScroll();
-    
-        $(document).scroll(debounce(function(){
-          animeScroll();
-        }, 200));
-        
-      });
-      
-    
-    });
+//
 
 
+document.addEventListener("DOMContentLoaded", function (event) {
 
-    //
 
+  var acc = document.getElementsByClassName("accordion");
+  var panel = document.getElementsByClassName('panel');
 
-    document.addEventListener("DOMContentLoaded", function(event) { 
-  
-  
-      var acc = document.getElementsByClassName("accordion");
-      var panel = document.getElementsByClassName('panel');
-      
-      for (var i = 0; i < acc.length; i++) {
-          acc[i].onclick = function() {
-              var setClasses = !this.classList.contains('active');
-              setClass(acc, 'active', 'remove');
-              setClass(panel, 'show', 'remove');
-      
-              if (setClasses) {
-                  this.classList.toggle("active");
-                  this.nextElementSibling.classList.toggle("show");
-              }
-          }
+  for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function () {
+      var setClasses = !this.classList.contains('active');
+      setClass(acc, 'active', 'remove');
+      setClass(panel, 'show', 'remove');
+
+      if (setClasses) {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
       }
-      
-      function setClass(els, className, fnName) {
-          for (var i = 0; i < els.length; i++) {
-              els[i].classList[fnName](className);
-          }
-      }
-      
-      });
+    }
+  }
+
+  function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+      els[i].classList[fnName](className);
+    }
+  }
+
+});
